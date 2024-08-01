360122a5a60241f706aada612b4b628c794eaeff
maxamedjaamac34 <shideoomaar10@gmail.com>
2024-08-01 13:08:56 -0500

    old code

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-let randomnumber = Math.floor(Math.random() * 100) + 1;
+let randomenumber = Math.floor(Math.random() * 100) + 1;
 
-function makeGuess() {
-    const userGuess = document.getElementById('guess-input').value;
-    const message = document.getElementById('message');
-    if(userGuess ==randomnumber){
-        message.textContent = "Congratulations! You guessed the correct number!";
-        message.style.color = 'green';
-        resetGame();
-    }else if(userGuess > randomnumber) {
-        message.textContent = 'Too high buddy! try again';
-        message.style.color = 'red';
-    }else if(userGuess  < randomnumber) {
-        message.textContent = 'Too low buddy! try again';
-        message.style.color = 'red';
-    }
-}
-function resetGame(){
-    randomnumber = Math.floor(Math.random() * 100) + 1;
-    document.getElementById('guess-input').value ='';
-}
+const guesses = document.querySelector(".guesses");
+const = lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const = guessSubmit = document.querySelector(".guessSubmit");
+const = guessField = document.querySelector(".guessField");
+
+
+
+let guessCount = 1;
+let resetButton;
